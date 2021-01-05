12c3a652de4a778f25d6c685cb6e169fdb6af7ff
shashishajin <t107590021@ntut.org.tw>
2021-01-06 01:10:03 +0800

    admin_merchandise finished not for api test yet

--- src/components/Admin_merchandise.vue
+++ src/components/Admin_merchandise.vue
@@ -10,59 +10,67 @@
                 <th>ITEM_PHOTO</th>
                 <th>ITEM_NAME</th>
                 <th>ITEM_DESCRIPTION</th>
+                <th>ITEM_TYPE</th>
                 <th>PRICE</th>
                 <th>QUANTITY</th>
               </tr>
               </thead>
               <tbody>
-                <form>
-                  <tr>
-                    <td>
-                        <input type="file" accept="image/*" @change="uploadImage($event)" id="MImage">
-                    </td>
-                    <td>
-                        <v-text-field
-                        type = "text"
-                        class = "font-weight-bold"
-                        outlined
-                        clearable
-                        v-model="MName"
-                        ></v-text-field>
-                    </td>
-                    <td>
-                        <v-text-field
-                        type = "text"
-                        class = "font-weight-bold"
-                        outlined
-                        clearable
-                        v-model="MDescption"
-                        ></v-text-field>
-                    </td>
-                    <td>
-                        <v-text-field
-                        type = "text"
-                        class = "font-weight-bold"
-                        outlined
-                        clearable
-                        v-model="MPrice"
-                        ></v-text-field>
-                    </td>
-                    <td>
-                        <v-text-field
-                        type = "text"
-                        class = "font-weight-bold"
-                        outlined
-                        clearable
-                        v-model="MQuantity"
-                        ></v-text-field>
-                    </td>
-                  </tr>
-                </form>
+                <tr>
+                  <td>
+                      <input type="file" accept="image/*" @change="uploadImage" id="MImage">
+                  </td>
+                  <td>
+                      <v-text-field
+                      type = "text"
+                      class = "font-weight-bold"
+                      outlined
+                      clearable
+                      v-model="MName"
+                      ></v-text-field>
+                  </td>
+                  <td>
+                      <v-text-field
+                      type = "text"
+                      class = "font-weight-bold"
+                      outlined
+                      clearable
+                      v-model="MDescption"
+                      ></v-text-field>
+                  </td>
+                  <td>
+                      <v-text-field
+                      type = "text"
+                      class = "font-weight-bold"
+                      outlined
+                      clearable
+                      v-model="MType"
+                      ></v-text-field>
+                  </td>
+                  <td>
+                      <v-text-field
+                      type = "text"
+                      class = "font-weight-bold"
+                      outlined
+                      clearable
+                      v-model="MPrice"
+                      ></v-text-field>
+                  </td>
+                  <td>
+                      <v-text-field
+                      type = "text"
+                      class = "font-weight-bold"
+                      outlined
+                      clearable
+                      v-model="MQuantity"
+                      ></v-text-field>
+                  </td>
+                </tr>
               </tbody>
             </template>
           </v-simple-table>
           <div class="col-md-12 col-sm-12 col-xs-12">
-            <v-btn class="primary white--text" outlined tile dense v-on:click="uploadMerchandise">送出</v-btn>
+            <v-btn class="primary white--text" outlined tile dense v-on:click="onUploadChange">送出</v-btn>
           </div>
         </div>
         
@@ -75,26 +83,31 @@
     import axios from 'axios'
     export default {
       name:"Admin_merchandise",
+      data(){
+        return{
+          selectedImage:null,
+          image:null
+        }
+      },
       methods: {
-        onUploadChange: async function(file) {
+        uploadImage(event){
+          this.selectedImage = event.target.files[0];
+          const reader = new FileReader(); //建立FileReader 監聽 Load 事件
+          reader.addEventListener('load',this.imageLoader);
+          reader.readAsDataURL(this.selectedImage);
+        },
+        imageLoader(event){
+          this.image=event.target.result;
+        },
+        onUploadChange:function() {
             var dataForm = new FormData();
-            const isIMAGE =
-                file.raw.type === "image/jpeg" || file.raw.type === "image/png";//判別格式
-            const isLt1M = file.size / 1024 / 1024 < 500000;//判別大小
-            if (!isIMAGE) {
-                this.$message.error("只能上傳jpg,png格式");
-                return false;
-            }
-            if (!isLt1M) {
-                this.$message.error("上傳圖片大小不能大於5MB");
-                return false;
-            }
-            this.picBase64 = await setBase64(file); //這邊將base64存入變數(setBase64()額外實作)
-            dataForm.append('file', event.target.files[0]);
+            dataForm.append('MerchandiseImage', this.image);  //base64
             dataForm.append('MerchandiseName', this.MName);
             dataForm.append('MerchandiseDescription', this.MDescription);
+            dataForm.append('MerchandiseType', this.MType);
             dataForm.append('MerchandisePrice', this.MPrice);
             dataForm.append('MerchandiseQuantity', this.MQuantity);
+            console.log(this.image);
             var config = {
               method:'post',
               url:'http://localhost:3000/Admin_merchandise',
